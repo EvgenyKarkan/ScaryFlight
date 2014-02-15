442da08560f4dbcfaf1908b37ecd7b6f9656dda0
Apple <arcilite.kd@gmail.com>
2014-02-15 21:43:46 +0200

    refactor complete

---
+++ ScaryFlight/ScaryFlight/Classes/Scenes/EABaseGameScene.h
@@ -0,0 +1,13 @@
+//
+//  EABaseGameScene.h
+//  ScaryFlight
+//
+//  Created by Artem Kislitsyn on 2/15/14.
+//  Copyright (c) 2014 EvgenyKarkan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EABaseGameScene : SKScene
+
+@end
