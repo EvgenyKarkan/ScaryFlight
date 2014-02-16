10e19085783ffc9c316f7a97ea0d7efa825ee037
Apple <arcilite.kd@gmail.com>
2014-02-16 16:09:41 +0200

    ground implement

--- ScaryFlight/ScaryFlight/Classes/Scenes/EARocketGameScene.m
+++ ScaryFlight/ScaryFlight/Classes/Scenes/EARocketGameScene.m
@@ -16,6 +16,14 @@
 
 #import "EAHero.h"
 
+static uint32_t const kHeroCategory   = 0x1 << 0;
+static uint32_t const kGroundCategory = 0x1 << 2;
+
+@interface EARocketGameScene ()
+
+@property (nonatomic, strong) SKSpriteNode *ground;
+
+@end
 
 @implementation EARocketGameScene;
 
@@ -31,10 +39,18 @@
 - (void)addBottom
 {
    
-    SKSpriteNode *background = [SKSpriteNode spriteNodeWithColor:[SKColor grayColor] size:CGSizeMake(self.size.width, 20)];
-    background.position = CGPointMake(0, 0);
-    background.name = @"empty";
-    [self addChild:background];
+    self.ground = [SKSpriteNode spriteNodeWithColor:[SKColor grayColor] size:CGSizeMake(self.size.width, 30.0f)];
+    //self.ground.size = CGSizeMake(self.size.width, 30.0f);
+    self.ground.centerRect = CGRectMake(26.0f / 20.0f, 26.0f / 20.0f, 4.0f / 20.0f, 4.0f / 20.0f);
+    self.ground.xScale = self.size.width / 20.0f;
+    self.ground.zPosition = 1.0f;
+    self.ground.position = CGPointMake(self.size.width / 2.0f, self.ground.size.height / 2.0f-self.ground.size.height);
+    self.ground.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:self.ground.size];
+    self.ground.physicsBody.categoryBitMask = kGroundCategory;
+    self.ground.physicsBody.collisionBitMask = kHeroCategory;
+    self.ground.physicsBody.affectedByGravity = NO;
+    self.ground.physicsBody.dynamic = NO;
+    [self addChild:self.ground];
 }
 
 
