2e9feceef6a77249b8f5b414904d5bf333c629ae
Evgeny Karkan <evgenykarkan@gmail.com>
2014-02-16 17:27:12 +0200

    Fixed static analyzer warning.

--- ScaryFlight/ScaryFlight/Classes/Scenes/EARocketGameScene.m
+++ ScaryFlight/ScaryFlight/Classes/Scenes/EARocketGameScene.m
@@ -44,7 +44,12 @@
 
 -(void)addBottomPipe:(float)centerY{
     [super addBottomPipe:centerY];
-    EAObstacle * pipeBottom = [self performSelector:@selector(pipeBottom)];
+    
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wundeclared-selector"
+     EAObstacle * pipeBottom = [self performSelector:@selector(pipeBottom)];
+#pragma clang diagnostic pop
+   
     CGFloat pipeBottomHeight = self.size.height - (centerY + (kPipeGap / 2.0f));
     [pipeBottom moveObstacleWithScale:(pipeBottomHeight ) / kPipeWidth];
     pipeBottom.position = CGPointMake(self.size.width + (pipeBottom.size.width / 2.0f), (pipeBottom.size.height / 2.0f));
